8320d764d49dff1a0e7b17db24ee9e821efba7e5
unknown <noamboy2006@gmail.com>
2019-08-03 23:45:05 +0900

    commit

--- views/src/index.js
+++ views/src/index.js
@@ -1,26 +1,13 @@
-console.log('Seoa WEB EJS Made by Seoa Development Team / PMH.')
-
+console.log('Seoa WEB EJS Made by Seoa Development Team / PMH,dissolve,Teaddy')
 const render = document.getElementById('render')
-
-let cycles = []
-
-for (let n = 0; n < document.getElementsByClassName('cycle').length; n++) {
-  cycles[n] = document.getElementsByClassName('cycle')[n].innerText.trim()
-}
-
-let cycle = 0
-setInterval(() => {
-  if (cycle >= cycles.length) cycle = 0
-  renderer(cycle)
-  cycle++
-}, 3000)
-
-function renderer (m) {
+let typing = document.getElementById('typing').innerHTML
+renderer()
+function renderer () {
   render.innerHTML = ''
   let temp = ''
-  for (let n = 0; n < cycles[m].split('').length; n++) {
+  for (let n = 0; n < typing.split('').length; n++) {
     setTimeout(() => {
-      temp += cycles[m].split('')[n]
+      temp += typing.split('')[n]
       render.innerHTML = '<i class="fas fa-quote-left"></i> ' + temp + ' <i class="fas fa-quote-right"></i>'
     }, 100 * n)
   }
